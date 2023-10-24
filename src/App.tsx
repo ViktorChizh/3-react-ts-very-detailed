69afea18f18535c32a645d80b4d495d881c37bac
ViktorChizh <victor.chizh@mail.ru>
2023-10-24 21:51:59 +0300

    class01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>HELLO!</h1>
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      <Star />
     </div>
   );
 }
 
 export default App;
+
+const Star = () => {
+  return (
+    <div>
+      ⭐️
+    </div>
+  )
+}
